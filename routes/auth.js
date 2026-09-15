7fd6a960c73db50b4d403d194151bdfd7c865aee
DomRadomski <domradomski123@gmail.com>
2026-09-15 15:36:11 +0300

    finished auth config

---
+++ routes/auth.js
@@ -0,0 +1,42 @@
+import express from "express";
+import bcrypt from "bcryptjs";
+import passport from "../config/passport.js";
+import { PrismaClient } from "../generated/prisma/client.js";
+
+const router = express.Router();
+const prisma = new PrismaClient();
+
+// Register
+router.post("/register", async (req, res, next) => {
+  try {
+    const { username, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    await prisma.user.create({
+      data: { username, password: hashedPassword },
+    });
+
+    res.redirect("/login");
+  } catch (err) {
+    next(err);
+  }
+});
+
+// Login
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/login",
+  })
+);
+
+// Logout
+router.post("/logout", (req, res, next) => {
+  req.logout((err) => {
+    if (err) return next(err);
+    res.redirect("/");
+  });
+});
+
+export default router;
